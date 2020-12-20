99c688044f2bce1ed0f4fe50beccdd6e1c59b8d9
Данил <danil.plavnik@mail.ru>
2020-12-20 23:09:52 +0400

    hi

--- main.py
+++ main.py
@@ -43,28 +43,28 @@ def start_screen():
                   "Правила работы в приложении:",
                   "  Не перенагружайте цепи!!!", "  Сохраняйте проект"]
 
-        fon = pygame.transform.scale(load_image('fon22.jpg'), (width, height))
-        screen.blit(fon, (0, 0))
-        font = pygame.font.Font(None, 40)
-        text_coord = 100
-        for line in intro_text:
-            string_rendered = font.render(line, 1, pygame.Color('black'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 100
-            text_coord += intro_rect.height
-            screen.blit(string_rendered, intro_rect)
-        clock = pygame.time.Clock()
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    terminate()
-                elif event.type == pygame.KEYDOWN or \
-                        event.type == pygame.MOUSEBUTTONDOWN:
-                    return  # начинаем игру
-            pygame.display.flip()
-            clock.tick(FPS)
+    fon = pygame.transform.scale(load_image('fon22.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 40)
+    text_coord = 100
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 100
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 start_screen()
