3189cdf52f5ffea409a68bae686f62c06f7e3330
Артем <artemmakrenko05205@gmail.com>
2020-12-27 21:11:59 +0400

    Поле v1.0

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class GameStates(Enum):
     STOP_SCREEN = 4
 
 
-WIDTH, HEIGHT = 700, 700
+WIDTH, HEIGHT = 1200, 900
 tile_width = tile_height = 50
 FPS = 30
 pygame.init()
@@ -86,28 +86,40 @@ def start_screen():
         clock.tick(FPS)
 
 
+def change_group(elements, manage):
+    for i in range(len(elements)):
+        pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((10, i * 40 + 40), (150, 35)),
+            text=elements[i],
+            manager=manage)
+
+
 def game_screen():
     global state
     pygame.display.set_caption("Start")
-    window_surface = pygame.display.set_mode((800, 600))
-    background = pygame.Surface((800, 600))
+    window_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    background = pygame.Surface((WIDTH, HEIGHT))
     color = (0, 0, 0)
     background.fill(color)
-    manage = pygame_gui.UIManager((800, 600))
-    green = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((350, 275), (100, 50)), text='green',
+    manage = pygame_gui.UIManager((WIDTH, HEIGHT))
+    '''green = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((0, 150), (100, 50)), text='green',
         manager=manage)
     red = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((150, 275), (100, 50)), text='red',
+        relative_rect=pygame.Rect((0, 250), (100, 50)), text='red',
         manager=manage)
     blue = pygame_gui.elements.UIButton(
-        relative_rect=pygame.Rect((550, 275), (100, 50)), text='blue',
-        manager=manage)
+        relative_rect=pygame.Rect((0, 350), (100, 50)), text='fon22.jpg',
+        manager=manage)'''
     res = data.select(['Type'], 'Groups')
     group = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(
         options_list=res, starting_option=res[0],
         relative_rect=pygame.Rect((10, 10), (150, 25)), manager=manage
     )
+    change_group(data.select(['title'], 'Elements', 'and', ["type", res[0]]),
+                 manage)
+    pygame.draw.rect(background, (50, 150, 50),
+                     (200, 0, WIDTH - 200, HEIGHT), width=0)
     entry = pygame_gui.elements.UITextEntryLine(
         relative_rect=pygame.Rect((350, 100), (100, 25)), manager=manage)
     clock = pygame.time.Clock()
@@ -119,7 +131,7 @@ def game_screen():
                 conf_dialog = pygame_gui.windows.UIConfirmationDialog(
                     rect=pygame.Rect((250, 200), (300, 200)), manager=manage,
                     window_title="Подтверждение",
-                    action_long_desc='Вы уверены?', action_short_name='OK',
+                    action_long_desc='Вы уверены?', action_short_name='Да',
                     blocking=False)
             elif event.type == pygame.USEREVENT:
                 if event.user_type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
@@ -127,22 +139,26 @@ def game_screen():
                 elif event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
                     terminate()
                 elif event.user_type == pygame_gui.UI_BUTTON_ON_HOVERED:
-                    if event.ui_element == green:
+                    '''if event.ui_element == green:
                         color = (0, 255, 0)
                     elif event.ui_element == red:
                         color = (255, 0, 0)
                     elif event.ui_element == blue:
-                        color = (0, 0, 255)
+                        color = (0, 0, 255)'''
                 elif event.user_type == pygame_gui.UI_BUTTON_ON_UNHOVERED:
                     color = (0, 0, 0)
                 elif event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
                     print(event.text)
+                    change_group(data.select(['title'], 'Elements', 'and',
+                                             ["type", event.text]), manage)
                 background.fill(color)
+                pygame.draw.rect(background, (50, 150, 50),
+                                 (200, 0, WIDTH - 200, HEIGHT), width=0)
             manage.process_events(event)
         manage.update(time_delta)
         window_surface.blit(background, (0, 0))
         manage.draw_ui(window_surface)
-        pygame.display.update()
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
